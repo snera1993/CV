3618860b3b4215cb6e55cc13c7c8b4f267350e60
snera1993 <notyhawat@gmail.com>
2018-08-17 15:45:59 +0200

    added smooth scroll

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("#about").on('click', function() {
+  
+        $('html, body').animate({
+          scrollTop: $('#toAbout').offset().top
+        }, 1000)
+    });
+  });
+
+
+
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("#education").on('click', function() {
+  
+        $('html, body').animate({
+          scrollTop: $('#toEducation').offset().top
+        }, 1000)
+    }); 
+  });
+
+
+
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("#work").on('click', function() {
+  
+        $('html, body').animate({
+          scrollTop: $('#toWork').offset().top
+        }, 1000)
+    }); 
+  });
+
+
+
+
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("#contact").on('click', function() {
+  
+        $('html, body').animate({
+          scrollTop: $('#toContact').offset().top
+        }, 1000)
+    }); 
+  });
